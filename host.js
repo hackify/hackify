d73d9a71201f1b0f1cad7dc67291b9a23d050b0d
Michael Dausmann <mdausmann@gmail.com>
2013-07-06 17:29:03 +1000

    refactor event names
    refactor event names and handler layouts so they are consistent and
    intuitive

--- host.js
+++ host.js
@@ -3,42 +3,40 @@ var fs = require('fs'),
     chokidar = require('chokidar');
 
 var config = JSON.parse(fs.readFileSync('config.json'));
+console.log('config:' + JSON.stringify(config));
 
 var socket = io.connect(config.serverURL);
 
 socket.on('connect', function(){
   console.log('connected to server ' + config.serverURL)
 
-  //server --> host (server requests complete file data for initialisation or when active editor chooses to change current file)
-  socket.on('changeFile', function(newFile){
+  socket.on('changeCurrentFile', function(newFile){
+    console.log('changeCurrentFile :' + newFile);
     fs.readFile(newFile, 'utf8', function(err, data) {
       if(err){
-        console.log('error reading file ' + newFile + ' ' + err);
+        socket.emit('newChatMessage', "error reading file " + newFile + ' ' + err);
       }else{
-        socket.emit('replaceData', data)
+        socket.emit('refreshData', data, true);
       }
     });
   });
 
-  socket.on('saveFile', function(data){
-    console.log('saveFile :' + JSON.stringify(data));
+  socket.on('saveCurrentFile', function(data){
+    console.log('saveCurrentFile :' + data.file);
     fs.writeFile(data.file, data.body, function(err){
       if(err){
-        socket.emit('sendchat', "error saving file " + data.file + ' ' + err);
+        socket.emit('newChatMessage', "error saving file " + data.file + ' ' + err);
       }else{
-        socket.emit('sendchat', "file save succeeded for file " + data.file);
+        socket.emit('newChatMessage', "file save succeeded for file " + data.file);
       }
     });
-  });
+  });  
 
-  console.log('config:' + JSON.stringify(config));
-
-  //host --> server (host requests creation of a room)
-  socket.emit('createOrJoinRoom', {
+  socket.emit('createRoom', {
     name: config.name
   });
 
-  //set up watch on folders
+  //set up watch on folders (Note that this will do an initial scan and emit as well as watch going forward)
   chokidar.watch(config.hostFolder, {ignored: new RegExp(config.ignoreRegex), persistent: true})
     .on('add', function(file) {socket.emit('fileAdded', file);})
     .on('change', function(file) {socket.emit('fileChanged', file);})
--- server.js
+++ server.js
@@ -21,67 +21,126 @@ function handler (req, res) {
 var rooms = {};
 
 io.sockets.on('connection', function (socket) {
-  //server --> client (tell the new client about available rooms)
+  //*****************************************
+  //********* Room Management ***************
+  //*****************************************
+
   //host --> server --> host (host sends metadata and a request to create a new room, Host in return gets a request to get fresh file data)
-  socket.on('createOrJoinRoom', function (data) {
+  socket.on('createRoom', function (data) {
     socket.set('room', data.name);
     socket.set('userId', 'host');
     socket.join(data.name);
     if(!rooms[data.name]){
       //full update of state including ody and request fresh file (pick first file arbitrarily)
       rooms[data.name] = data;
-
-      // socket.emit('changeFile', data.files[0]);
       rooms[data.name].files = [];
-      // rooms[data.name].currentFile = data.files[0];
       rooms[data.name].currentFile = null;
       rooms[data.name].hostSocket = socket;
 
       console.log('new room created:' + data.name);
     }else{
       //just update the files
-      // rooms[data.name].files = data.files;
       rooms[data.name].hostSocket = socket;
       console.log('host reconnected to room' + data.name);
     }   
   });
 
+  //client --> server --> client (client joins a particular room and gets room data refreshed)
+  socket.on('joinRoom', function (data) {
+    console.log('recieved Join request room' + data.room);
+    if(rooms[data.room]){
+      //set up the socket properties
+      socket.join(data.room);
+      socket.set('room', data.room);
+      var userId = (data.userId)?data.userId:'hacker' + Math.floor(Math.random() * 9999999999).toString();
+      socket.set('userId', userId);
+
+      //tell the socket about the room state
+      var roomState = rooms[data.room];
+      roomState.files.forEach(function(file){
+        socket.emit('fileAdded', file)
+      });
+      socket.emit('changeCurrentFile', roomState.currentFile);
+      socket.emit('refreshData', roomState.body);
+
+      //tell this socket about all of the users (including itself)
+      var clients = io.sockets.clients(data.room);
+      clients.forEach(function(client){
+        client.get('userId', function(err, clientUserId){
+          if(clientUserId){
+            socket.emit('newUser', {userId:clientUserId, isYou:(client===socket)?true:false});
+          }
+        });
+      });      
+
+      //now tell all of the other sockets about the new user
+      socket.broadcast.to(data.room).emit('newUser', {userId:userId, isYou:false});
+    }else{
+      socket.emit('newChatMessage','room ' + data.room + ' does not exist', 'hackify')
+    }
+  });
+
+  //client --> server (client leaves a particular room)
+  socket.on('leaveRoom', function (data) {
+    socket.leave(data.room);
+    socket.set('room', null);
+    //todo - tell everybody else in room that user is gone.. also wire up same to socket disconnect...?? both??
+  });
+
+
+  //*****************************************
+  //************ Code Editing ***************
+  //*****************************************
+
   //host --> server (send hosted file data to the server for collaborative editing)
   //client --> server (active editor sends a full copy of the modified file data to the server)
-  socket.on('refreshData', function (body_) {
+  socket.on('refreshData', function (body, broadcast) {
     socket.get('room', function (err, room) {
       console.log('new body');
-      rooms[room].body = body_;      
+      rooms[room].body = body;
+      if(broadcast){
+        socket.broadcast.to(room).emit('refreshData', body);
+      }   
     });
   });
 
-  //host --> sever --> client (host sends new file data which is then broadcast to clients)
-  socket.on('replaceData', function (body_) {
+  //client --> server --> clients (active editor sends incremental file data change to the server which then broadcasts it to other clients in the same room)
+  socket.on('changeData', function (op) {
     socket.get('room', function (err, room) {
-      rooms[room].body = body_;
-      socket.broadcast.to(room).emit('refreshData', body_);
+      if(!err && room!="" && room !=null){
+        console.log(op);
+        if (op.origin == '+input' || op.origin == 'paste' || op.origin == '+delete') {
+          socket.broadcast.to(room).emit('changeData', op);
+        };        
+      }
     });
-  });
+  });  
+
+
+  //*****************************************
+  //********** File Management **************
+  //*****************************************
 
   //client --> server --> client/host (client requests a change to the file which is passed on to the host so it can load the file and the clients so they can change the file heading)
-  socket.on('changeFile', function(newFile){
+  socket.on('changeCurrentFile', function(newFile){
     socket.get('room', function (err, room) {
       if(!err && room!="" && room !=null){
         rooms[room].currentFile = newFile;
-        io.sockets.in(room).emit('changeFile', newFile);
+        io.sockets.in(room).emit('changeCurrentFile', newFile);
       }
     });    
   });
 
-  socket.on('saveFile', function(){
+  //client --> server --> Host (Client requests a file save, server tells the host to do it)
+  socket.on('saveCurrentFile', function(){
     socket.get('room', function (err, room) {
       if(!err && room!="" && room !=null){
-        rooms[room].hostSocket.emit('saveFile', {file:rooms[room].currentFile, body:rooms[room].body})
+        rooms[room].hostSocket.emit('saveCurrentFile', {file:rooms[room].currentFile, body:rooms[room].body})
       }
     });    
   });
 
-  //watch operations from host
+  //host --> server --> client (Host watches its file system and notifies server of changes, server passes this on to clients if its validated)
   socket.on('fileAdded', function (file) {
     socket.get('room', function (err, room) {
       if(!err && room!="" && room !=null){
@@ -114,67 +173,17 @@ io.sockets.on('connection', function (socket) {
     });
   });  
 
-  //client --> server --> client (client joins a particular room and gets room data refreshed)
-  socket.on('join', function (data) {
-    console.log('recieved Join request room' + data.room);
-    if(rooms[data.room]){
-      //set up the socket properties
-      socket.join(data.room);
-      socket.set('room', data.room);
-      var userId = (data.userId)?data.userId:'hacker' + Math.floor(Math.random() * 9999999999).toString();
-      socket.set('userId', userId);
-
-      //tell the socket about the room state
-      var roomState = rooms[data.room];
-      roomState.files.forEach(function(file){
-        socket.emit('fileAdded', file)
-      });
-      socket.emit('changeFile', roomState.currentFile);
-      socket.emit('refreshData', roomState.body);
-
-      //tell this socket about all of the users (including itself)
-      var clients = io.sockets.clients(data.room);
-      clients.forEach(function(client){
-        client.get('userId', function(err, clientUserId){
-          if(clientUserId){
-            socket.emit('newUser', {userId:clientUserId, isYou:(client===socket)?true:false});
-          }
-        });
-      });      
-
-      //now tell all of the other sockets about the new user
-      socket.broadcast.to(data.room).emit('newUser', {userId:userId, isYou:false});
-    }else{
-      socket.emit('updatechat','hackify','room ' + data.room + ' does not exist')
-    }
-  });
-
-  //client --> server (client leaves a particular room)
-  socket.on('leave', function (data) {
-    socket.leave(data.room);
-    socket.set('room', null);
-    //todo - tell everybody else in room that user is gone.. also wire up same to socket disconnect...?? both??
-  });
-
-  //client --> server --> clients (active editor sends incremental file data change to the server which then broadcasts it to other clients in the same room)
-  socket.on('change', function (op) {
-    socket.get('room', function (err, room) {
-      if(!err && room!="" && room !=null){
-        console.log(op);
-        if (op.origin == '+input' || op.origin == 'paste' || op.origin == '+delete') {
-          socket.broadcast.to(room).emit('change', op);
-        };        
-      }
-    });
-  });
+  //*****************************************
+  //********** Chat Messages **************
+  //*****************************************
 
   //client --> server --> clients (chat message from client)
-  socket.on('sendchat', function (data) {
+  socket.on('newChatMessage', function (data) {
     socket.get('room', function (err, room) {
       if(!err && room!="" && room !=null){
         socket.get('userId', function(err, userId){
-          console.log('sendchat:' + data);
-          io.sockets.in(room).emit('updatechat',userId, data);          
+          console.log('newChatMessage:' + data);
+          io.sockets.in(room).emit('newChatMessage', data, userId);          
       })
       }
     });
@@ -187,7 +196,7 @@ io.sockets.on('connection', function (socket) {
         socket.get('userId', function(err, userId){
           socket.set('userId', newUserId, function(){
             io.sockets.in(room).emit('userIdChanged',userId, newUserId);
-            io.sockets.in(room).emit('updatechat', 'hackify', userId + ' changed name to ' + newUserId);              
+            io.sockets.in(room).emit('newChatMessage', userId + ' changed name to ' + newUserId, 'hackify');              
           });
         
       })
