66baf4ede1aafd6da17cc981afb6642723b5ec66
Michael Dausmann <mdausmann@gmail.com>
2013-07-08 19:16:14 +1000

    Command Line and NPM Ready

--- README.md
+++ README.md
@@ -7,32 +7,34 @@ Like plug.dj for collaborative development
 You are hacking away on your laptop, listening to music, you are smashing it, if only you could share this experience...
 You spin up a terminal... 
 ```
-$ node host.js
+$ npm install hackify -g
+$ cd /Users/awesomeguy/cool_project
+$ hackify
 ```
 BAM! you have hosted a room, a collaborative coding space... No setup, No logins, Your code, on Your machine.
-You send the room link to your code bro's.... http://hackify.org?room=myAwesomeProject
+You send the room link to your code bro's.... http://hackify.org?room=123456
 They all hit the link... BAM! your all coding together
 You chat, you code, you listen to the same music, your coding awesomeness is now a shared experience.
 Congratulations you've been Hackified.
 
-## Components
-* server.js - runs in the cloud, socket, room state, broadcast.
-* host.js - runs on the box of the 'host', requests room creation, manages file info and updates
-* index.html - served by server.js, simple client, file editing, messaging
-* config.json - read by host.js defines room name and folders to host and ignore. (yeah ok there is a tiny bit of setup, quit whining)
+## Command Line Goodness
 
-## Prerequisites (host/server)
+  Usage: hackify [options]
+
+  Options:
+
+    -h, --help             output usage information
+    -V, --version          output the version number
+    -n, --name [name]      Name for the room
+    -p, --pass [password]  Moderator password for the room
+    -s, --server [url]     Location of the hackify server [http://www.hackify.org]
+    -i, --ignore [regex]   Regex of folders to ignore [/(node_modules|.git)/]
+    -r, --readonly         Read Only
+
+
+## Prerequisites
 * Node.js
 
-## Running in Dev
-Clone repo then..
-```
-$ npm install
-$ node server.js
-$ node host.js (seperate terminal)
-```
-this will effectively 'host' the hackify project in a local instance of hackify (very circular)
 
-using 2 or more browsers, browse to... [http://localhost:4000?room=hackify](http://localhost:4000?room=hackify)
 
 
--- host.js
+++ host.js
@@ -1,17 +1,29 @@
+#!/usr/bin/env node
+
 var fs = require('fs'),
     io = require('socket.io-client'),
-    chokidar = require('chokidar');
+    chokidar = require('chokidar'),
+    program = require('commander');;
 
-var config = JSON.parse(fs.readFileSync('config.json'));
-console.log('config:' + JSON.stringify(config));
+program
+  .version('0.1.0')
+  .option('-n, --name [name]', 'Name for the room', Math.floor(Math.random() * 999999).toString())
+  .option('-p, --pass [password]', 'Moderator password for the room', Math.floor(Math.random() * 999999).toString())    
+  .option('-s, --server [url]', 'Location of the hackify server [http://www.hackify.org]', 'http://www.hackify.org')
+  .option('-i, --ignore [regex]', 'Regex of folders to ignore [/(node_modules|.git)/]', '/(node_modules|.git)/')  
+  .option('-r, --readonly', 'Read Only')
+  .parse(process.argv);
 
-var moderatorPass = (process.argv.length>=3)?process.argv[2]:'';
-console.log('moderatorPass:' + moderatorPass);
+console.log('Creating a hackify room with:');
+console.log('room link: %s?room=%s', program.server, program.name);
+console.log('moderator password: %s', program.pass);
+console.log('ignoring folders matching: %s', program.ignore);
+if (program.readonly) console.log('room will be read only');
 
-var socket = io.connect(config.serverURL);
+var socket = io.connect(program.server);
 
 socket.on('connect', function(){
-  console.log('connected to server ' + config.serverURL)
+  console.log('connected to server ' + program.server)
 
   socket.on('changeCurrentFile', function(newFile){
     console.log('changeCurrentFile :' + newFile);
@@ -36,12 +48,12 @@ socket.on('connect', function(){
   });  
 
   socket.emit('createRoom', {
-    name: config.name,
-    moderatorPass: moderatorPass
+    name: program.name,
+    moderatorPass: program.pass
   });
 
   //set up watch on folders (Note that this will do an initial scan and emit as well as watch going forward)
-  chokidar.watch(config.hostFolder, {ignored: new RegExp(config.ignoreRegex), persistent: true})
+  chokidar.watch(process.cwd(), {ignored: new RegExp(program.ignore), persistent: true})
     .on('add', function(file) {socket.emit('fileAdded', file);})
     .on('change', function(file) {socket.emit('fileChanged', file);})
     .on('unlink', function(file) {socket.emit('fileDeleted', file);})
